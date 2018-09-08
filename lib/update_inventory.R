234212c936c494a597fdc5e535e4ea0e06227f00
guz84 <ian@hersberger.ws>
2018-09-08 10:44:07 +0200

    functions to update tables after buying, cooking, rejecting items

---
+++ lib/update_inventory.R
@@ -0,0 +1,48 @@
+# Source File - table update functions
+#
+#Structure INVENTOR:    Name | Amount | Unit | DateBought
+#                       char | double | char | POSIXct
+#
+
+
+#add bought item to inventory
+# 
+# @param: inventory:   dataframe INGREDIENTS    :(data.frame)
+# @param: name:        Name of item bought item :(char)
+# @param: amount:      Amount of item bought    :(double)
+# @param: unit:        Unit of item bought      :(char)
+# @param: date_bought: Date Bought              :(POSIXct)
+
+update_inventory_buy <- function(inventory, name, amount, unit, date=Sys.Date()){
+  
+  ##Todo: Unit Converison here
+  #
+  #amount = unit_convert(amount, unit)
+  #
+  
+  if (unit == inventory[inventory$Name == name, "Date"] == date){
+    inventory[inventory$Name == name, "Amount"] = inventory[inventory$Name == name, "Amount"] + amount
+  } else {
+    inventory(rbind(inventory, data.frame(Name=name, Amount=amount, Unit=unit, DateBought=date)))
+  }
+  inventory
+}
+
+
+# subtract the ingredients used for cooking from inventory list and return the updated inventory
+# 
+# @param: inventory:           dataframe INVENTORY    :(data.frame)
+# @param: cooking_ingredients  dataframe INVENTORY    :(data.frame)
+
+# subtracts the amounts used in the recipe from the inventory and returns the updated inventory
+
+update_inventory_cook <- function(inventory, cooking_ingredients){
+  ##todo unit convert line
+  #cooking_ingredients = unit_convert_line()
+  #
+  for (ingredient in cooking_ingredients){
+    ## what if there is an item with multiple occurrences
+    inventory[inventory$Name==ingredient$Name, "Amount"] = max(inventory[inventory$Name==ingredient$Name, "Amount"] - ingredient$Amount,0) 
+  }
+  inventory
+}
