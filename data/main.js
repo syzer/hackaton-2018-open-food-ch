f3204b47ebd06baaab23e32d9194c8d45228bba8
syzer3@gmail.com <syzer3@gmail.com>
2018-09-08 02:23:38 +0200

    here i fixed it :D

--- data/main.js
+++ data/main.js
@@ -1,25 +1,25 @@
-const log = m => console.log(m);
+const log = m => console.log(m)
 
 // https://food.schoolofdata.ch/food-allergens/
-const allergenes = require("./allergene.json");
+const allergenes = require("./allergene.json")
 // https://open-api.digimeals.com/recipes/?language=de_CH
-const rezepte = require("./rezepte.json");
+const rezepte = require("./rezepte.json")
 
-const Untergruppe = ({ Untergruppe }) => Untergruppe;
-const id = ({ id }) => id;
-const name = ({ name }) => name;
-const includesAllergene = name => allergene.includes(name); // could be fuzzy matching...
-const hasMatches = arr => arr.isAllergen.length > 0;
+const Untergruppe = ({ Untergruppe }) => Untergruppe
+const id = ({ id }) => id
+const name = ({ name }) => name
+const includesAllergene = name => allergene.includes(name) // could be fuzzy matching...
+const hasMatches = arr => arr.isAllergen.length > 0
 
-const allergene = allergenes.map(Untergruppe); //.map(log);
+const allergene = allergenes.map(Untergruppe) //.map(log)
 
 const ids = rezepte
   .map(id)
   .map(id => {
-    const r = require(`./rezepte/${id}.json`);
-    const isAllergen = r.ingredientsArr.map(name).filter(includesAllergene);
-    return { id, isAllergen };
+    const r = require(`./rezepte/${id}.json`)
+    const isAllergen = r.ingredientsArr.map(name).filter(includesAllergene)
+    return { id, isAllergen }
   })
-  .filter(hasMatches);
+  .filter(hasMatches)
 
-console.log(ids);
+console.log(ids)
