b53a14b25706b7e158b2b73526a7d02851aadd1a
guz84 <ian@hersberger.ws>
2018-09-08 19:59:48 +0200

    catered to id input wish

--- lib/update_expiry.R
+++ lib/update_expiry.R
@@ -10,11 +10,12 @@
 # @param: expiry_table: table of expected life span of products (data.frame)
 # @param: item: item id                                         (int)
 # @param: change_reason [spoiled / still_good]                  (char)
-update_expiry_table <- function(expiry_table, item, change_reason){
+update_expiry_table <- function(expiry_table, inventory, id, date_bought, change_reason){
+  item = inventory[inventory$id == id & inventory$DateBought==date_bought,]
   if (change_reason =="spoiled"){
-    expiry_table[expiry_table$id == item$id, "expiry"] = expiry_table[expiry_table$id == item$id, "avg_days"]-1 
+    expiry_table[expiry_table$id == item$id, "expiry"] = expiry_table[expiry_table$id == item$id, "expiry"]-1 
   } else if (change_reason == "still_good"){
-    expiry_table[expiry_table$id == item$id, "expiry"] = expiry_table[expiry_table$id == item$id, "avg_days"]+1
+    expiry_table[expiry_table$id == item$id, "expiry"] = expiry_table[expiry_table$id == item$id, "expiry"]+1
   }
   expiry_table
 }
