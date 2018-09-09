b3066dae4d145b59ca3bd503ac46980dc9688aaa
guz84 <ian@hersberger.ws>
2018-09-09 09:51:35 +0200

    add buy/in stock field

--- lib/get_close_and_recommend.R
+++ lib/get_close_and_recommend.R
@@ -1,5 +1,10 @@
 
 
+## deal with case where ingredient is not in expiry table
+## either set days_till_expiry manually in that case
+## or skip execution in loop entirely
+
+
 get_list_of_items2use <- function(inventory, expiry_table, date, cutoff=3){
   days = c()
   cutoff_items = c()
@@ -13,7 +18,6 @@ get_list_of_items2use <- function(inventory, expiry_table, date, cutoff=3){
     days_till_expiry = expiry_table[expiry_table$id == item$id, "expiry"]-difftime(date, item$DateBought, units="days")
    # days_till_expiry = date + diff.Date(item$DateBought, expiry_table[expiry_table$id == item$id, "expiry"])
     if(days_till_expiry<cutoff){
-      print(ix)
       cutoff_items=c(cutoff_items, days_till_expiry)
     }
     item$days_till_expiration =days_till_expiry
@@ -43,6 +47,16 @@ get_viable_recipes <- function(recipe_list, ingredients_to_use, eaters){
   #  put_back = recipe_list[order(score_vec,decreasing=T)][1:3]
   put_back = recipe_list[order(score_vec,decreasing=T)][[1]]
   list(put_back$groupsArr$title, put_back$groupsArr$imgUrl, put_back$ingredientsArr)
+}
+
+
+
+get_instock_buy_table <- function(inventory, recipe_ingredients){
+  ISBchar <- rep("Buy", nrow(recipe_ingredients))
+  instock_indices = recipe_ingredients$id %in% inventory$id
+  ISBchar[instock_indices] <- "in Stock"
+  recipe_ingredients$Availability <- ISBchar
+  recipe_ingredients[order(recipe_ingredients$Availability, decreasing=T),]
 }
 
 #some tests
